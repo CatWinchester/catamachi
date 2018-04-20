e104a442ee4ec6836dc33742e36df8844a054464
Kate Shadryna <chackloonka@Kates-MacBook-Pro.local>
2018-04-20 17:13:54 +0300

    ran npm eject=> now able to change webpack config add scss and styles to cat

--- src/GameContainer/components/Cat/Cat.js
+++ src/GameContainer/components/Cat/Cat.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
+import styles from './Cat.module.scss'
 
 const Cat = (props) => {
-  return <img src={props.imageUrl} alt="Here should be cat"/>
+  return <div className={styles.cat}>
+    <img src={props.imageUrl} alt="Here should be cat"/>
+  </div>
 }
 
 Cat.propTypes = {
