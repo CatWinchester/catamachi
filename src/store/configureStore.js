365bc1dd289192243c108222356d950b7616c337
Kate Shadryna <chackloonka@Kates-MacBook-Pro.local>
2018-04-27 12:47:51 +0300

    add redux devtools compatibility

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,6 +1,6 @@
 import { createStore, applyMiddleware } from 'redux'
 import createSagaMiddleware from 'redux-saga'
-
+import { composeWithDevTools } from 'redux-devtools-extension'
 import rootSaga from '../GameContainer/sagas'
 import rootReducer from '../GameContainer/reducers'
 
@@ -9,7 +9,9 @@ export default function configureStore(initialState) {
   const store = createStore(
     rootReducer,
     initialState,
-    applyMiddleware(sagaMiddleware)
+    composeWithDevTools(
+      applyMiddleware(sagaMiddleware)
+    ),
   )
 
   store.runSaga = sagaMiddleware.run(rootSaga)
